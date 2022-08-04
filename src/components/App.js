2243e7aa227f4f01faa166ec7fb7b7f8a6681204
tanferest <tanferest@gmail.com>
2022-08-04 11:00:11 +0200

    create header component

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,16 @@
+import { useState } from 'react';
+import products from '../data/products.json';
 
+import Header from './Header';
 
 const App = () => {
-  return <></>;
+  const [product, setProduct] = useState(products);
+
+  return (
+    <>
+      <Header></Header>
+    </>
+  );
 };
 
 export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,7 @@
+function Header () {
+    return <header className="header">
+        <h1 className="header__title">Top Stockouts Review</h1>
+    </header>
+}
+
+export default Header;
